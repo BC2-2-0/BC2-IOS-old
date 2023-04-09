203c05950a69cc194bff53e0fbfb8fb37f6aeea6
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2023-04-09 15:13:31 +0900

    initial commit

---
+++ BC2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BC2
+//
+//  Created by AnnKangHo on 2023/04/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+
+
+}
+
