540e85a6d7d87a7b7a4eeddb49f33cde25b250a7
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2023-05-17 23:17:45 +0900

    sse 통신 완료

---
+++ BC2/Data/MyData.swift
@@ -0,0 +1,13 @@
+//
+//  MyData.swift
+//  BC2
+//
+//  Created by AnnKangHo on 2023/05/17.
+//
+
+import Foundation
+
+class MyData {
+    static var shared = MyData()
+    var moneyValue: String = " "
+}
--- BC2/View/MainViewController.swift
+++ BC2/View/MainViewController.swift
@@ -6,13 +6,29 @@
 //
 
 import UIKit
+import EventSource
+
+struct MoneyData: Decodable {
+    let balance: String
+}
+
+var kkkkk: String = " "
+var responseBalance: Int = 0
+var check: Int = 0
+let myData = MyData.shared
 
 class MainViewController: BaseVC {
     
     var userName: String = " "
     var userEmail: String = " "
     
+    var balances: String = " "
+    
+    var kkkk: String = " "
+
     var amount = 0
+    
+    var moneyCount: Int = 0
     
     private let headerView = Header()
     
@@ -34,7 +50,7 @@ class MainViewController: BaseVC {
     }
     
     private let miniBlock = BaseVC().block
-
+    
     private let paymentButton = PaymentButton()
     
     private let miningButton = PaymentButton().then{
@@ -86,10 +102,15 @@ class MainViewController: BaseVC {
         $0.setTitleColor(UIColor.black, for: .normal)
         $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14.0)
     }
-    
+    private var eventSource: EventSource?
+
     override func addView() {
         changeNameLabel()
         changeAmountLabel()
+        let myData = MyData.shared
+        myData.moneyValue = String(responseBalance)
+        responseBalance = Int(myData.moneyValue)!
+        serverSendEvent()
         view.addSubview(headerView)
         view.addSubview(miniBlock)
         view.addSubview(sub)
@@ -177,8 +198,6 @@ class MainViewController: BaseVC {
             self.coinRechargeLabel.alpha = 0
             self.coinImageView.alpha = 0
             self.rechargeImageView.alpha = 0
-//            self.goMiningButton.alpha = 0
-//            self.goRechargeButton.alpha = 0
         }) { _ in
             self.popUpView.removeFromSuperview()
             self.backgroundView.removeFromSuperview()
@@ -239,10 +258,7 @@ class MainViewController: BaseVC {
     }
     
     func changeAmountLabel() {
-        let moneyFormatter: NumberFormatter = NumberFormatter()
-        moneyFormatter.numberStyle = .decimal
-        let result: String = moneyFormatter.string(for: amount)! + " 원"
-        boxInLabel.amountLabel.text = result
+        boxInLabel.amountLabel.text = kkkk
     }
     
     func changeNameLabel() {
@@ -259,6 +275,61 @@ class MainViewController: BaseVC {
         
         headerView.userNameLabel.attributedText = attributedText
     }
+    
+    func serverSendEvent(){
+        print("ASD")
+        
+        let eventSourceURL = "http://13.125.77.165:3000/receive"
+//        let result: String = String(kkkkk)
+//        boxInLabel.amountLabel.text = result
+        let eventSource = EventSource(request: .init(url: URL(string: eventSourceURL)!))
+        self.eventSource = eventSource
+        print(myData.moneyValue)
+        eventSource.connect()
+        Task {
+            for await event in eventSource.events {
+                switch event {
+                case .open:
+                    print("성공")
+                    print("Connection was opened. main")
+                case .error(let error):
+                    print("에러")
+                    print("Received an error:", error.localizedDescription)
+                case .message(let message):
+                    do {
+                        let response = try JSONDecoder().decode(MoneyData.self, from: message.data!.data(using: .utf8)!)
+                        if check == 0 {
+                            myData.moneyValue = String(responseBalance)
+                            responseBalance = Int(myData.moneyValue)!
+                            print(myData.moneyValue)
+                            responseBalance = Int(response.balance)! - 100
+                            let result: String = String(responseBalance) + " 원"
+                            kkkkk = result
+                            boxInLabel.amountLabel.text = result
+                            check = 1
+                        }
+                        else {
+                            print(myData.moneyValue)
+                            myData.moneyValue = String(responseBalance)
+                            responseBalance = Int(myData.moneyValue)!
+                            responseBalance = responseBalance + 10
+                            let result: String = String(responseBalance) + " 원"
+                            kkkkk = result
+                            boxInLabel.amountLabel.text = result
+                            myData.moneyValue = String(Int(myData.moneyValue)! + 10)
+                            print(myData.moneyValue)
+                        }
+                    } catch {
+                        
+                    }
+                    print("메시지")
+                    print("Received a message", message.data ?? "데이터 없음")
+                case .closed:
+                    print("Connection was closed.")
+                }
+            }
+        }
+    }
 }
 extension MainViewController {
     
@@ -273,6 +344,16 @@ extension MainViewController {
         nextVC.userName = self.userName
         nextVC.userEmail = self.userEmail
         self.navigationController?.pushViewController(nextVC, animated: false)
+        self.coinRechargeLabel.isHidden = true
+        self.goMiningButton.isHidden = true
+        self.goRechargeButton.isHidden = true
+        self.coinImageView.isHidden = true
+        self.rechargeImageView.isHidden = true
+        self.backgroundView.isHidden = true
+        self.popUpView.isHidden = true
+//        Task {
+//            await eventSource?.close()
+//        }
     }
     
     @objc func showPopup() {
@@ -283,6 +364,8 @@ extension MainViewController {
         self.goRechargeButton.isHidden = false
         self.coinImageView.isHidden = false
         self.rechargeImageView.isHidden = false
+        self.backgroundView.isHidden = false
+        self.popUpView.isHidden = false
         self.backgroundView.frame = view.bounds
         popUpView.alpha = 0
         backgroundView.alpha = 0
